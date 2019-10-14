7af7d76a025a0baeb7c6b248dfda3c38f12b7a02
Vladimir Karetnikov <karetnikov.v.e@gmail.com>
2019-10-14 15:25:27 +0300

    task done

--- js/form.js
+++ js/form.js
@@ -7,6 +7,7 @@
     HOUSE: '5000',
     PALACE: '10000'
   };
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var submitBtn = window.map.newOfferForm.querySelector('.ad-form__submit');
   var roomInput = window.map.newOfferForm.querySelector('#room_number');
@@ -15,6 +16,10 @@
   var timeout = window.map.newOfferForm.querySelector('#timeout');
   var appartmentType = window.map.newOfferForm.querySelector('#type');
   var appartmentPrice = window.map.newOfferForm.querySelector('#price');
+  var avatarInput = document.querySelector('.ad-form-header__input');
+  var avatarPreview = document.querySelector('.ad-form-header__preview');
+  var apartmentInput = document.querySelector('.ad-form__input');
+  var apartmentPhotoPreview = document.querySelector('.ad-form__photo');
 
   var validateGuestsAndRooms = function () {
     var rooms = roomInput.value;
@@ -84,6 +89,45 @@
     document.addEventListener('click', closeMsg);
   };
 
+  var removeImages = function () {
+    avatarPreview.querySelector('img').src = 'img/muffin-grey.svg';
+    apartmentPhotoPreview.textContent = '';
+  };
+
+  var appendImage = function (reader, preview) {
+    var img = document.createElement('img');
+    img.src = reader.result;
+    img.height = 100;
+    preview.appendChild(img);
+  };
+
+  var onFileUpload = function (input, preview, appendItems) {
+    return function () {
+      var file = input.files[0];
+      var fileName = file.name.toLowerCase();
+
+      var matches = FILE_TYPES.some(function (item) {
+        return fileName.endsWith(item);
+      });
+
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          if (appendItems && preview.querySelector('img')) {
+            appendImage(reader, preview);
+          } else if (!preview.querySelector('img')) {
+            appendImage(reader, preview);
+          } else {
+            preview.querySelector('img').src = reader.result;
+          }
+        });
+
+        reader.readAsDataURL(file);
+      }
+    };
+  };
+
   roomInput.addEventListener('change', onRoomsOrGuestsInput);
   guestsInput.addEventListener('change', onRoomsOrGuestsInput);
   timein.addEventListener('change', syncTimeOut);
@@ -92,6 +136,7 @@
   submitBtn.addEventListener('click', function () {
     onRoomsOrGuestsInput();
   });
+
   window.map.newOfferForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
     window.backend.save(window.map.Url.SAVE, new FormData(window.map.newOfferForm), function () {
@@ -99,4 +144,15 @@
       showSuccessMsg();
     }, window.backend.onError);
   });
+
+  window.map.newOfferForm.addEventListener('reset', function () {
+    removeImages();
+  });
+
+  apartmentInput.addEventListener('change', onFileUpload(apartmentInput, apartmentPhotoPreview, true));
+  avatarInput.addEventListener('change', onFileUpload(avatarInput, avatarPreview, false));
+
+  window.form = {
+    removeImages: removeImages
+  };
 })();
